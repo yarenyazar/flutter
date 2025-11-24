3747a5126566407aff5bb6939a20a2e5eeb50eb2
yaren-yazar <yarenyazar67@gmail.com>
2025-11-24 15:30:11 +0300

    renamed

---
+++ flutter1/lib/degisken_olusturma.dart
@@ -0,0 +1,11 @@
+void main() {
+  var ogrenciAdi = "Yaren";
+  var ogrenciYasi = 23;
+  var ogrenciBoyu = 1.78;
+  var ogrenciBasHarfi = "Y";
+
+  print(ogrenciAdi);
+  print(ogrenciYasi);
+  print(ogrenciBoyu);
+  print(ogrenciBasHarfi);
+}
