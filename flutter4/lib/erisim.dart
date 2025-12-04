f19838e9def63d5f1eb6717e0f9b1c9c3feea014
Yaren Yazar <yarenyazar67@gmail.com>
2025-12-04 19:08:18 +0300

    feat: add Erisim class with public and private variable examples

---
+++ flutter4/lib/erisim.dart
@@ -0,0 +1,4 @@
+class Erisim {
+  late int publicDegisken;
+  late int _privateDegisken; // private değişkene sadece sınıf içinden erişim sağlayabiliriz public değişkene ise her yerden erişim sağlarız
+}
---
+++ flutter4/lib/erisim_main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter4/erisim.dart';
+
+void main() {
+  var e = Erisim();
+  e.publicDegisken = 1;
+  // e.privateDegisken = 2;  bu ifade hata verir çünkü private bir değişken
+}
