062c380c258771a4ee84a50754045135e8f3ae6a
Yaren Yazar <yarenyazar67@gmail.com>
2025-12-09 14:53:00 +0300

    feat: add functionality to input and display personnel information

--- flutter5/lib/list_composition_ornek.dart
+++ flutter5/lib/list_composition_ornek.dart
@@ -1,6 +1,26 @@
+import 'dart:io';
 import 'package:flutter5/personel.dart';
+import 'adres.dart';
 
 void main() {
   var personeller = <Personel>[];
+  for(var i = 1; i < 4; i++) {
+    stdout.write("$i. Personelin adını giriniz: ");
+    String isim = stdin.readLineSync()!;
+    stdout.write("$i. Personelin adres il bilgisini giriniz: ");
+    String il = stdin.readLineSync()!;
+    stdout.write("$i. Personelin adres ilçe bilgisini giriniz: ");
+    String ilce = stdin.readLineSync()!;
 
+    var adres = Adres(il, ilce);
+    var personel = Personel(i, isim, adres);
+    personeller.add(personel);
+  }
+  for(var p in personeller) {
+    print("***************");
+    print("Personel No: ${p.no}");
+    print("Personel Adı: ${p.isim}");
+    print("Personel Adres İli: ${p.adres.il}");
+    print("Personel Adres İlçesi: ${p.adres.ilce}");
+  }
 }
