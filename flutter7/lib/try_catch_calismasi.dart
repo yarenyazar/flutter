4e86a450c09978ab8acb221d10251f4f4d3c16a9
Yaren Yazar <yarenyazar67@gmail.com>
2026-01-19 11:35:06 +0300

    docs: add comments explaining try-catch usage in Kotlin

---
+++ flutter7/lib/try_catch_calismasi.dart
@@ -0,0 +1,6 @@
+/*
+1.Derleme sırasında oluşabilecek hatalar için kullanılır
+2.Genelde kotlin input-output işlemleri için kullanılır yani veri alış veriş işlemlerinde kullanılır
+2.Kullanılacak yer mutlaka hata fırlatmalıdır
+ */
+
