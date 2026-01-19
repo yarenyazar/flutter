56fb5cdd06c493e9aec32134cde6ecfb092d3840
Yaren Yazar <yarenyazar67@gmail.com>
2026-01-19 17:23:47 +0300

    feat: add try-catch example for handling list index out of range in Dart

--- flutter7/lib/try_catch_calismasi.dart
+++ flutter7/lib/try_catch_calismasi.dart
@@ -4,3 +4,16 @@
 2.Kullanılacak yer mutlaka hata fırlatmalıdır
  */
 
+void main() {
+  var sayilar = <int>[];
+  sayilar.add(34); // 0. indeks
+  sayilar.add(67); // 1. indeks
+
+  try {
+    sayilar[2] = 89;
+    print("İşlem tamam"); // hatanın olduğu satırdan sonrasını çalıştırmadı çünkü hatayı alınca direkt catche atlar
+  }
+  catch(e) {
+    print("Listenin boyutunu aştınız!");
+  }
+}
